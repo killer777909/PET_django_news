b3bc9cc615d65e20cd18b15f2c13b0039d445678
killer777909 <killer777909@rambler.ru>
2022-09-28 20:20:08 +0500

    the form for adding news has been replaced with a model-dependent one

--- news/forms.py
+++ news/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название:', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='Текст:', required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
-    is_published = forms.BooleanField(label='Опубликовано:', initial=True, required=False)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория:',
-                                      empty_label='Выберите категорию',
-                                      widget=forms.Select(attrs={'class': 'form-control'}))
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'})
+        }
--- news/models.py
+++ news/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
