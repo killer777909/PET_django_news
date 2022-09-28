c6c001c14248d471c38ff9a04023f28b5af05430
killer777909 <killer777909@rambler.ru>
2022-09-28 17:29:43 +0500

    actual WIP version

---
+++ news/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.urls import reverse
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='наименование')
+    content = models.TextField(blank=True, verbose_name='содержимое')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['title']
