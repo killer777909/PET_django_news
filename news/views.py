5da9459cf506395ebbb1e7a8ae79bb6c94756f4f
killer777909 <killer777909@rambler.ru>
2022-09-30 00:36:28 +0500

    removed hello world from home page

--- news/views.py
+++ news/views.py
@@ -31,7 +31,7 @@ class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    mixin_prop = 'hello world'
+    # mixin_prop = 'hello world'
     paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
